dbbf1e9cbb04f46f817b71d2cd9324d5d00ad39e
bheikes-isu <bheikes@iastate.edu>
2025-11-17 19:10:03 -0600

    Open AI

--- GUI.py
+++ GUI.py
@@ -9,7 +9,7 @@ activity = st.selectbox("Select your activity", WIP2.activities)
 lat,long= WIP2.getcoordinates(place)
 weatherdata, hourly_dataframe, current_data= WIP2.getweather(lat,long)
 st.write(f"Current Temperature in {place}: {weatherdata}°F")
-st.write("Suggested outfit is:", WIP2.weather(weatherdata))
+st.write(WIP2.chatoutput(activity, weatherdata))
 
 #Trying to incorporate the Plotly weather graph into Streamlit
 #fig_linechart = px.line(hourly_dataframe, x="hour", y="temperature_2m", markers=True)
